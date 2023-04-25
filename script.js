a0b1b706212a7a20fc12786a03ef9735bb667db0
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-04-25 15:48:31 +0100

    Updated App, localStorage function added

--- script.js
+++ script.js
@@ -7,33 +7,16 @@ const deleteBtn = document.getElementById("deleteBtn");
 const noteGrid = document.getElementById("noteGrid");
 const gridItem1 = document.getElementById("gridItem1");
 
-function addNote() {
-  let html = `
-    <div class="grid-item-1 note" id="grid-item-1">
-        <p class="note-text">
-        ${textArea.value}
-        </p>
-        <div class="delete-btn" id="deleteBtn">
-        X
-        </div>
-    </div>
-  `;
-  noteGrid.insertAdjacentHTML("afterbegin", html);
-  textArea.value = "";
-
-  textArea.focus();
-  textArea.setSelectionRange(0, 0);
-}
-
-document.body.addEventListener("click", function (e) {
-  if (e.target.id === "deleteBtn") e.target.closest(".note").remove();
-});
+let storageArr = localStorage.getItem("notes")
+  ? JSON.parse(localStorage.getItem("notes"))
+  : [];
 
 addNoteBtn.addEventListener("click", function () {
   if (textArea.value === "") {
     noTextAlert();
   } else {
-    addNote();
+    const note = textArea;
+    createNote(note);
   }
 });
 
@@ -41,6 +24,86 @@ window.addEventListener("keydown", function (e) {
   if (textArea.value === "" && e.key === "Enter") {
     noTextAlert();
   } else if (e.key === "Enter") {
-    addNote();
+    const note = textArea;
+    createNote(note);
   }
 });
+
+function createNote(note) {
+  // Store item in storageArr
+  storageArr.push(note.value);
+  updateLocalStorage();
+
+  // Reset the cursor
+  textArea.focus();
+  textArea.setSelectionRange(0, 0);
+
+  // refresh page
+  location.reload();
+
+  // Clear the text area
+  textArea.value = "";
+}
+
+function displayNotes() {
+  let notes = "";
+  for (let i = 0; i < storageArr.length; i++) {
+    notes += `
+                <div class="grid-item-1 note" id="grid-item-1">
+                    <p class="note-text cat">
+                    ${storageArr[i]}
+                    </p>
+                    <div class="delete-btn" id="deleteBtn">
+                    X
+                    </div>
+                </div>
+  `;
+  }
+  noteGrid.insertAdjacentHTML("afterbegin", notes);
+  activateDeleteListener();
+}
+
+function deleteItem(i) {
+  storageArr.splice(i, 1);
+  updateLocalStorage();
+  // location.reload()
+}
+
+function activateDeleteListener() {
+  let deleteBtn = document.querySelectorAll(".delete-btn");
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener("click", function () {
+      deleteItem(i);
+    });
+  });
+}
+
+document.body.addEventListener("click", function (e) {
+  if (e.target.id === "deleteBtn") {
+    e.target.closest(".note").remove();
+  }
+});
+
+function noTextAlert() {
+  alert("Please write something in the text area");
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("notes", JSON.stringify(storageArr));
+}
+
+displayNotes();
+console.log(storageArr);
+
+// Create an object for each note.
+// const note = `
+//   <div class="grid-item-1 note" id="grid-item-1">
+//       <p class="note-text">
+//       ${textArea.value}
+//       </p>
+//       <div class="delete-btn" id="deleteBtn">
+//       X
+//       </div>
+//   </div>
+// `;
+// noteGrid.insertAdjacentHTML("afterbegin", note);
